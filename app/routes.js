281a645c319ffa4771be71847057f2ab2b7f82cc
Jose Zamudio <zamudio@outlook.com>
2015-01-27 22:53:03 -0800

    Update routes.js with signup form processing

--- app/routes.js
+++ app/routes.js
@@ -47,4 +47,12 @@ module.exports = function(app, passport) {
     // if they aren't redirect them to the home page
     res.redirect('/');
   }
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash message
+  }));
+
 }
