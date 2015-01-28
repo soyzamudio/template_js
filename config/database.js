a607f2d117626d1f8fa63b87b6e7227d91e79575
Jose Zamudio <zamudio@outlook.com>
2015-01-27 21:12:56 -0800

    Set up database.js and update body-parser

---
+++ config/database.js
@@ -0,0 +1,4 @@
+// config/database.js
+module.exports = {
+  'url' : 'mongodb://<user>:<pass>@proximus.modulusmongo.net:27017/yh2yMumu' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
